bd3e54a28b06519d0c6fddfb9edab5325be9d27f
Carlos Contreras <carlos@thoroughcare.net>
2021-01-04 23:48:00 -0500

    TTY gem template

---
+++ lib/obli.rb
@@ -0,0 +1,6 @@
+require "obli/version"
+
+module Obli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/obli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Obli do
+  it "has a version number" do
+    expect(Obli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
